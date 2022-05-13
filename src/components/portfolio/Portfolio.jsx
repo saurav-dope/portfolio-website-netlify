445e1d63fc0fb026ebf83c8ca18d6bc854dcd87c
Saurav Dutta <sauravdutta1270@gmail.com>
2022-05-13 11:07:05 +0530

    Added calclator app in portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import "./portfolio.css";
-import IMG1 from "../../assets/insta.jpg";
-import IMG2 from "../../assets/todolist.jpg";
-import IMG3 from "../../assets/simple_static.jpg";
+import React from 'react';
+import './portfolio.css';
+import IMG1 from '../../assets/calculator.jpg';
+import IMG2 from '../../assets/todolist.jpg';
+import IMG3 from '../../assets/simple_static.jpg';
 
 // Array of Portfolio Images
 
@@ -10,26 +10,25 @@ const data = [
   {
     id: 1,
     image: IMG1,
-    title: "Instagram Clone",
-    github:
-      "https://github.com/saurav-dope/My-First-React-Assignments/tree/main/Assignment-4/code",
-    demo: "Specify Live link For Project Here",
+    title: 'Calculator App',
+    github: 'https://github.com/saurav-dope/Calculator',
+    demo: 'Specify Live link For Project Here',
   },
   {
     id: 2,
     image: IMG2,
-    title: "Todo List App",
+    title: 'Todo List App',
     github:
-      "https://github.com/saurav-dope/My-First-HTML-Assignment/tree/Assignment-6/src/Assignment-6",
-    demo: "Specify Live link For Project Here",
+      'https://github.com/saurav-dope/My-First-HTML-Assignment/tree/Assignment-6/src/Assignment-6',
+    demo: 'Specify Live link For Project Here',
   },
   {
     id: 3,
     image: IMG3,
-    title: "Simple Static Website",
+    title: 'Simple Static Website',
     github:
-      "https://github.com/saurav-dope/My-First-HTML-Assignment/tree/Assignment-4/src/Assignment-4",
-    demo: "Specify Live link For Project Here",
+      'https://github.com/saurav-dope/My-First-HTML-Assignment/tree/Assignment-4/src/Assignment-4',
+    demo: 'Specify Live link For Project Here',
   },
 ];
 
@@ -43,27 +42,27 @@ const Portfolio = ({ setAlert }) => {
   };
 
   return (
-    <section id="portfolio">
+    <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
-      <div className="container portfolio__container">
+      <div className='container portfolio__container'>
         {data.map(({ id, image, title, github, demo }) => {
           return (
-            <article key={id} className="portfolio__item">
-              <div className="portfolio__item-image">
+            <article key={id} className='portfolio__item'>
+              <div className='portfolio__item-image'>
                 <img src={image} alt={title} />
               </div>
               <h3>{title}</h3>
-              <div className="portfolio__item-cta">
-                <a href={github} className="btn">
+              <div className='portfolio__item-cta'>
+                <a href={github} className='btn'>
                   Github
                 </a>
                 <a
-                  href="/"
-                  className="btn btn-primary"
-                  target="_blank"
-                  rel="noreferrer"
+                  href='/'
+                  className='btn btn-primary'
+                  target='_blank'
+                  rel='noreferrer'
                   onClick={handleClick}
                 >
                   Live Demo
